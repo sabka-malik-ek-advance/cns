8ddd3466f185964f035446deb2498ecf53ec6d7c
Aditya Trivedi <adityatrivedi612@gmail.com>
2022-10-30 22:05:13 +0530

    Add files via upload

--- clang/Caesar.cpp
+++ clang/Caesar.cpp
@@ -1,60 +1,41 @@
+
 // A C++ program to illustrate Caesar Cipher Technique
 #include <iostream>
 using namespace std;
 
-string newtext = "";
 // This function receives text and shift and
 // returns the encrypted text
-string encrypt(string text, int k)
+string encrypt(string text, int s)
 {
-    string encr = "";
+	string result = "";
 
-    // traverse text
-    for (int i = 0; i < text.length(); i++)
-    {
-        // apply transformation to each character
-        // Encrypt Uppercase letters
-        if (isupper(text[i]))
-            encr += char(int(text[i] + k - 65) % 26 + 65);
+	// traverse text
+	for (int i = 0; i < text.length(); i++) {
+		// apply transformation to each character
+		// Encrypt Uppercase letters
+		if (isupper(text[i]))
+			result += char(int(text[i] + s - 65) % 26 + 65);
 
-        // Encrypt Lowercase letters
-        else
-            encr += char(int(text[i] + k - 97) % 26 + 97);
-    }
+		// Encrypt Lowercase letters
+		else
+			result += char(int(text[i] + s - 97) % 26 + 97);
+	}
 
-    // Return the resulting string
-    return encr;
-}
-
-string decrypt(string newtext, int k)
-{
-    string decr = "";
-
-    // traverse text
-    for (int i = 0; i < newtext.length(); i++)
-    {
-        // apply transformation to each character
-        // Encrypt Uppercase letters
-        if (isupper(newtext[i]))
-            decr += char(int(newtext[i] + k - 65) % 26 + 65);
-
-        // Encrypt Lowercase letters
-        else
-            decr += char(int(newtext[i] + k - 97) % 26 + 97);
-    }
-
-    // Return the resulting string
-    return decr;
+	// Return the resulting string
+	return result;
 }
 
 // Driver program to test the above function
 int main()
 {
-    string text = "ATTACKATONCE";
-    int k = 4;
-    cout << "Text : " << newtext;
-    cout << "\nShift: " << k;
-    cout << "\nEncrypted: " << encrypt(text, k);
-    cout << "\nDecrypted: " << decrypt(newtext, k);
-    return 0;
+    string text;
+	cout<<"Enter the Message : ";
+    cin>>text;
+	int s;
+	cout << "Text : " << text;
+    cout<<"\nEnter the Shift in Numbers to encrypt: ";
+    cin>>s;
+	cout << "\nShift: " << s;
+	cout << "\nCipher text : " << encrypt(text, s);
+	return 0;
 }
